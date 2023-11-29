f3c054a26eb1a3b448f09ca24295bc7d9811a00c
BenitoLM <benito.lopez.tecno@gmail.com>
2023-11-28 19:30:25 -0600

    merge new brnach

--- resources/views/admin/auditoriaInternas/index.blade.php
+++ resources/views/admin/auditoriaInternas/index.blade.php
@@ -183,7 +183,7 @@
                         }
                     }
                 }
-                // dtButtons.push(deleteButton)
+                // dtButtons.push(deleteButton )
             @endcan
 
             let dtOverrideGlobals = {
